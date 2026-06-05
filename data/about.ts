a58aa915f0e9aa739e0f67793a1e4b6e73321595
Mohammad Valadi <movascript@gmail.com>
2026-06-05 14:38:49 +0330

    💬 updated dates to match more accuratly

--- data/about.ts
+++ data/about.ts
@@ -10,26 +10,30 @@ export const technologies = [
   "Preact",
   "Tauri",
   "Zod",
+  "Node.js",
   "Express",
+  "PostgreSQL",
   "WebRTC",
   "WebScoket",
   "PWA",
   "Docker",
   "Nginx",
+  "PNPM",
+  "CI/CD",
 ];
 
 export const experiences = [
   {
     title: "Frontend Engineer",
     company: "Nekoo Pardazan Sepehr",
-    period: "Dec 2023 — Aug 2024",
+    period: "May 2024 — Feb 2026",
     description:
       "Worked on a university PWA platform designed for students, professors, and staff. The platform included real-time chat systems, course scheduling, score tracking, and event management.",
   },
   {
     title: "Frontend Team Lead",
     company: "Olgoo",
-    period: "Aug 2024 — Jan 2025",
+    period: "Nov 2024 — Aug 2025",
     description:
       "Led frontend development for a startup focused on inventory management and analytics for clothing manufacturers. Built dashboards, analytics systems, and production management workflows.",
   },
